ea26518cfc4cab51333ed1cf6b649a5b27388f8d
leeor <leeor.aharon@gmail.com>
2015-07-26 08:59:11 +0300

    vi-mode settings

---
+++ plugins/vi-mode/vi-mode.plugin.zsh
@@ -0,0 +1,65 @@
+# Ensures that $terminfo values are valid and updates editor information when
+# the keymap changes.
+function zle-keymap-select zle-line-init zle-line-finish {
+  # The terminal must be in application mode when ZLE is active for $terminfo
+  # values to be valid.
+  if (( ${+terminfo[smkx]} )); then
+    printf '%s' ${terminfo[smkx]}
+  fi
+  if (( ${+terminfo[rmkx]} )); then
+    printf '%s' ${terminfo[rmkx]}
+  fi
+
+  zle reset-prompt
+  zle -R
+}
+
+# Ensure that the prompt is redrawn when the terminal size changes.
+TRAPWINCH() {
+  zle && { zle reset-prompt; zle -R }
+}
+
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
+zle -N edit-command-line
+
+# set vim editing mode
+bindkey -v
+# shorter timeout when leaving insert mode
+export KEYTIMEOUT=1
+
+# and bind some common navigation keys
+bindkey ^A vi-beginning-of-line
+bindkey ^E vi-end-of-line
+bindkey "OH" vi-beginning-of-line
+bindkey "OF" vi-end-of-line
+bindkey "[3~" vi-delete-char
+bindkey "OA" up-line-or-search
+bindkey "OB" down-line-or-search
+bindkey "^I" expand-or-complete-with-dots
+bindkey -M vicmd / vi-history-search-backward
+bindkey -M vicmd n vi-repeat-search
+bindkey -M vicmd N vi-rev-repeat-search
+
+# allow v to edit the command line (standard behaviour)
+#autoload -Uz edit-command-line
+#bindkey -M vicmd 'v' edit-command-line
+
+# allow ctrl-p, ctrl-n for navigate history (standard behaviour)
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+# allow ctrl-h, ctrl-w, ctrl-? for char and word deletion (standard behaviour)
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+
+# if mode indicator wasn't setup by theme, define default
+if [[ "$MODE_INDICATOR" == "" ]]; then
+  MODE_INDICATOR="%{$fg_bold[red]%}NORMAL%{$reset_color%}"
+fi
+
+function vi_mode_prompt_info() {
+  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
+}
